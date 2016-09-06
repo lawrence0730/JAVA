40a4f2d842c75ec1ea7c204feaa029e53daa91e1
Guan, chi-hao <chihauguan@gmail.com>
2016-09-06 16:48:54 +0800

    Use math class
    Use some math class member

---
+++ MathPractice.java
@@ -0,0 +1,13 @@
+package practicePkg;
+
+public class MathPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.PI);
+		System.out.println(Math.abs(1.11));
+		System.out.println(Math.abs(0.23));
+		System.out.println(Math.abs(3));
+	}
+
+}
